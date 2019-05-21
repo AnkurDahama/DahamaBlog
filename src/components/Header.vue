55d4a825648b908d6568973db9b9b01033c1aa7f
Ankur Dahama <ankur.dahama@kii.fi>
2019-05-21 14:00:08 +0300

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+<div id="header_main">
+  <h1> Welcome to Heading </h1>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<style scoped>
+h1 {
+  color: black;
+}
+</style>
